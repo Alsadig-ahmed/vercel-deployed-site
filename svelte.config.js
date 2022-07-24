0b35e4f0343c6478f301607d1aea0dce9f968181
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-07-24 12:19:44 +0200

    add and use vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
-import { mdsvex } from 'mdsvex'
+// import adapter from "@sveltejs/adapter-auto";
+import vercel from "@sveltejs/adapter-vercel";
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +11,22 @@ const config = {
     preprocess({ postcss: true }),
   ],
   kit: {
-    adapter: adapter(),
+    // adapter: adapter(),
+    // default options for vercel
+    adapter: vercel({
+      // if true, will deploy the app using edge functions
+      // (https://vercel.com/docs/concepts/functions/edge-functions)
+      // rather than serverless functions
+      edge: false,
+
+      // an array of dependencies that esbuild should treat
+      // as external when bundling functions
+      external: [],
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app
+      split: false,
+    }),
   },
 };
 
